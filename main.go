bc848ad97c46c1b7ea60e660b10bfeb3df738edf
dfchong <dfchong@sina.com>
2025-06-08 09:35:38 +0800

    1

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
-import(
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
 	tele "gopkg.in/telebot.v4"
 )
 
 func main() {
-	pref := tele.Settings{
-		
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load .env")
 	}
+	pref := tele.Settings{
+		Token: os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello",func(ctx tele.Context) error {
+		return ctx.Send("helloadf")
+	})
+
+	b.Start()
 }
--- object.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Post struct{
-	Id int `json:"id"`
-	Content string `json:"content`
-	Author string `json:"author`
-}
